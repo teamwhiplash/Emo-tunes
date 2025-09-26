c579b9b2415c555ad018f8edd5322645f4803528
Aldrin Antony <aldrinantonyofficial7@gmail.com>
2025-09-26 07:47:15 +0530

    Bugfix + Rebasing Commit (#3)
    * Update README.md
    
    * BugFix:Credential,previewURL,del-spotifyCred
    
    * Rebasing commit

--- src/main/java/org/tunes/config/Credentials.java
+++ src/main/java/org/tunes/config/Credentials.java
@@ -1,35 +1,27 @@
 package org.tunes.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
-@Configuration
-@ConfigurationProperties(prefix ="spotify")
+@Service
 public class Credentials {
-    private String CLIENT_ID;
-    private String CLIENT_SECRET;
-    private String REDIRECT;
+    private final String CLIENT_ID = System.getenv("CLIENT_ID");
+    private final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
+    private final String REDIRECT = System.getenv("REDIRECT_URI");
 
-    public String getCLIENT_ID(){
+    public String getCLIENT_ID() {
         return CLIENT_ID;
     }
 
-    public void setCLIENT_ID(String CLIENT_ID){
-        this.CLIENT_ID=CLIENT_ID;
-    }
-    public String getCLIENT_SECRET(){
+
+    public String getCLIENT_SECRET() {
         return CLIENT_SECRET;
     }
 
-    public void setCLIENT_SECRET(String CLIENT_SECRET){
-        this.CLIENT_SECRET= CLIENT_SECRET;
-    }
 
-    public String getREDIRECT(){
+    public String getREDIRECT() {
         return REDIRECT;
-    }
-    public void setREDIRECT(String REDIRECT){
-        this.REDIRECT=REDIRECT;
-    }
 
+
+    }
 }
