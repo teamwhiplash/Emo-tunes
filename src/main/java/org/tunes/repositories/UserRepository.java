a78e179bf94b8c9d31a4f82cb7b92d056cfc71a0
J-sankar <jayasankarmenonv@gmail.com>
2025-09-28 20:51:59 +0530

    create user repository interface

--- src/main/java/org/tunes/repositories/UserRepository.java
+++ src/main/java/org/tunes/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package org.tunes.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tunes.models.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findBySpotifyId(String spotifyId);
+    User findByEmail(String email);
+    User findByUsername(String username);
 }
