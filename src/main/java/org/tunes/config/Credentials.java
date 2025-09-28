f5df352679bbcbc2f183c04fcb430cebe2002e2a
J-sankar <jayasankarmenonv@gmail.com>
2025-09-28 20:46:50 +0530

    fix:issue in environment variable injection

--- src/main/java/org/tunes/config/Credentials.java
+++ src/main/java/org/tunes/config/Credentials.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Credentials {
+
     private final String CLIENT_ID = System.getenv("CLIENT_ID");
     private final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
     private final String REDIRECT = System.getenv("REDIRECT_URI");
