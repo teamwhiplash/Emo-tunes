8ad6974ca75b04b62c7825eef5cb51b416544e13
J-sankar <jayasankarmenonv@gmail.com>
2025-10-04 18:59:41 +0530

    add: playlist information dto

--- src/main/java/org/tunes/dto/PlaylistInfo.java
+++ src/main/java/org/tunes/dto/PlaylistInfo.java
@@ -1,4 +1,54 @@
 package org.tunes.dto;
 
+import java.util.List;
+
 public class PlaylistInfo {
+    private String playlistId;
+    private String playlistName;
+    private String coverUrl;
+    private List<SongInfo> songs;
+
+    public PlaylistInfo() {
+    }
+
+    public PlaylistInfo(String playlistId, String playlistName, String coverUrl, List<SongInfo> songs) {
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+        this.coverUrl = coverUrl;
+        this.songs = songs;
+    }
+
+    // Getters and setters
+    public String getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(String playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+    public List<SongInfo> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<SongInfo> songs) {
+        this.songs = songs;
+    }
+
 }
