ee92208b3cd5e7a5dff873187dffd34b8c2a607a
J-sankar <jayasankarmenonv@gmail.com>
2025-10-04 19:01:46 +0530

    fix:user id data type changes

--- src/main/java/org/tunes/repositories/UserRepository.java
+++ src/main/java/org/tunes/repositories/UserRepository.java
@@ -3,8 +3,9 @@ package org.tunes.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.tunes.models.Users;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<Users, Integer> {
     Users findBySpotifyId(String spotifyId);
     Users findByEmail(String email);
     Users findByUsername(String username);
+
 }
