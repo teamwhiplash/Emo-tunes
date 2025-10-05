c50de6f2e7c403732432f06f0fbe15d3538c0ede
Jayasankar <jayasankarmenonv@gmail.com>
2025-10-05 20:43:10 +0530

    Modify songs fetching endpoint to retrieve all song details (#10)

--- src/main/java/org/tunes/controllers/SpotifySearchController.java
+++ src/main/java/org/tunes/controllers/SpotifySearchController.java
@@ -16,7 +16,7 @@ import org.tunes.services.SpotifySearch;
 import java.util.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/search")
 public class SpotifySearchController {
     @Autowired
     private SpotifySearch spotifySearch;
@@ -31,19 +31,21 @@ public class SpotifySearchController {
     private TokenStore tokenStore;
 
 
-    @GetMapping("/search")
-    public ResponseEntity SearchSong(@RequestParam String query, @RequestParam Integer userId){
+    @GetMapping("/song")
+    public ResponseEntity SearchSong(@RequestParam String query, @RequestParam Integer userId,@RequestParam(defaultValue = "20") String limit,
+                                     @RequestParam(defaultValue = "0") String offset){
         String accessToken = tokenStore.getValidAccessToken(userId);
 
-    Map<String,Object> SongResponse = search.RequestSong(accessToken,query, new SpotifySearch.SearchSong());
-    SongInfo Song = mapper.toSongInfo(SongResponse);
+    Map<String,Object> SongResponse = search.RequestSong(accessToken,query, new SpotifySearch.SearchSong(),limit,offset);
+        List<SongInfo> songs = mapper.toSongInfoList(SongResponse);
+
             System.out.println("\n");
-            System.out.println(Song);
-            System.out.println("Song Name:"+ Song.getSongName()+"\n"+"Artist:"+ Song.getArtistName());
-            return ResponseEntity.ok().body(Song);
+//            System.out.println(Song);
+//            System.out.println("Song Name:"+ Song.getSongName()+"\n"+"Artist:"+ Song.getArtistName());
+            return ResponseEntity.ok().body(songs);
     }
 
-    @GetMapping("/search/playlist")
+    @GetMapping("/playlist")
     public ResponseEntity SearchPlaylist(@RequestParam String query, @RequestParam(defaultValue = "20") String limit,
     @RequestParam(defaultValue = "0") String offset, @RequestParam Integer userId) {
         String accessToken = tokenStore.getValidAccessToken(userId);
